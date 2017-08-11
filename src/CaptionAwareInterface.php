3a9205984c24ac1fefcd5508056ab9e6087064c4
Miguel Muscat <miguelmuscat93@gmail.com>
2017-08-11 11:19:32 +0200

    Added `CaptionAwareInterface`

---
+++ src/CaptionAwareInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dhii\Util\String;
+
+/**
+ * Something that is aware of a caption.
+ *
+ * @since [*next-version*]
+ */
+interface CaptionAwareInterface
+{
+    /**
+     * Retrieves the caption related to this instance.
+     *
+     * A message is a human-readable string that provides a brief description for some element.
+     * It typically accompanies some illustration or screen fragment.
+     *
+     * @since [*next-version*]
+     *
+     * @return string|StringableInterface
+     */
+    public function getCaption();
+}
